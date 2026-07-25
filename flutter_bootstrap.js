e7c26fa5f49232baf92b4fbac387d5fb5177f237
gmanolov.bul-dot <gmanolov.bul@gmail.com>
2026-07-25 11:53:13 +0300

    v9 xp levels leaderboard

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3613359332" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1062319608" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
